26baff1b186039865fcae9975dec4cd9c91a5528
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-03-12 21:20:00 -0500

    added HulahoopRepository to create SQL tables

---
+++ SpringHoops-1/src/main/java/dmacc/repository/HulahoopRepository.java
@@ -0,0 +1,10 @@
+package dmacc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dmacc.beans.Hulahoop;
+
+public interface HulahoopRepository extends JpaRepository <Hulahoop, Long> 
+{
+
+}
